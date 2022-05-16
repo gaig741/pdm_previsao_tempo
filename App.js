8e3aca612869a0aabbb43139e015113daa69ddab
gaig741 <85653706+gaig741@users.noreply.github.com>
2022-05-15 21:26:26 -0300

    interface gráfica inicial

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { 
+  Button,
+  FlatList,
+  StyleSheet, 
+  Text,
+  TextInput, 
+  View } from 'react-native';
+import {
+  LANGUAGE,
+  UNITS,
+  CNT,
+  APPID,
+  BASE_URL
+} from '@env'
 
 export default function App() {
+
+  const [previsoes, setPrevisoes] = useState([])
+
+
+  const [cidade, setCidade] = useState('')
+  const capturarCidade = (cidadeDigitada) => {
+    setCidade(cidadeDigitada)
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.containerView}>
+        <View style={styles.entradaView}>
+          <TextInput
+            style={styles.cidadeTextInput}
+            placeholder="Digite o nome de uma cidade"
+            value={cidade}
+            onChangeText={capturarCidade}      //1:05:00 DA AULA DO BOSSINI
+          />    
+          <Button 
+            title="OK"
+          />
+        </View>
+      <FlatList
+        data={previsoes}
+        renderItem={p => (
+          <Text>{JSON.stringify(p)}</Text>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  containerView:{
+    padding: 40,
   },
+  entradaView: {
+    marginTop: 8,
+  },
+  cidadeTextInput: {
+    padding: 12,
+    borderBottomColor: '#FF9800',
+    borderBottomWidth: 2,
+    marginBottom: 4
+  }
 });
